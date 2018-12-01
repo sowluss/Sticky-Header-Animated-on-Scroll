73833d5866561295769752e737dd51043c72a0ad
sowluss <sowluss@gmail.com>
2018-12-01 20:48:57 +0200

    Sticky Header Animated on Scroll with jQuery

---
+++ main.js
@@ -0,0 +1,9 @@
+$(window).scroll(function(){
+  let scroll = $(window).scrollTop();
+  if (scroll > 0){
+    $("header").addClass('scrolled');
+  }
+  else {
+    $("header").removeClass('scrolled');
+  }
+})
